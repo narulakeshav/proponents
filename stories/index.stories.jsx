0f75d91e043839d2342bc93bb74b5f69a3c82c21
narulakeshav <narulakeshav13@gmail.com>
2018-11-18 19:49:16 -0800

    Rename index.jsx to index.stories.jsx

---
+++ stories/index.stories.jsx
@@ -0,0 +1,14 @@
+/**
+ * Internal Dependencies
+ *
+ * @TODO: Use webpack config to automatically use the
+ * stories in the component folder.
+ */
+// Btn Stories
+import '../src/components/button/stories/index.stories';
+
+// Typography Stories
+import '../src/components/text/stories/index.stories';
+
+// Card Stories
+import '../src/components/card/stories/index.stories';
